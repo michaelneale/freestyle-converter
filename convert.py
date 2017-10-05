8745b5f122e3f3e65b67d2df166cd3c4bf01f88b
Michael Neale <michael.neale@gmail.com>
2017-10-05 18:48:58 +1100

    progress eh

--- convert.py
+++ convert.py
@@ -3,6 +3,28 @@ from bs4 import BeautifulSoup
 file = open('config.xml')
 soup = BeautifulSoup(file, 'lxml')
 
-repo = soup.find_all('hudson.plugins.git.userremoteconfig')[0].url.text
+project = soup.find('project')
+git_config = soup.find('hudson.plugins.git.userremoteconfig')
+label = soup.find('assignedname')
+artifact = soup.find('hudson.tasks.artifactarchiver')
+junit = soup.find('hudson.tasks.junit.junitresultarchiver')
 
-print repo
+if not project: 
+    exit("Not a freestyle project")
+
+print git_config
+print label
+#print soup.find('assignednamex')
+#print soup.find('command')
+builders = soup.find('builders')
+
+
+builder_converters = {    
+    'hudson.tasks.shell' : 'shell',
+    'hudson.tasks.batchfile' : 'bat',
+    'hudson.plugins.build__timeout.buildstepwithtimeout' : 'timeout'
+}
+
+for builder in builders:     
+    if builder.name and builder.name in builder_converters:         
+        print "----> " + builder_converters[builder.name]
